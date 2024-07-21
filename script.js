901a9d4d854cc9a46fdcb5ad612d9865dc2f3971
Don Bosco <don965877@gmail.com>
2024-07-21 23:10:13 +0300

    added html, css, & js files

---
+++ script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+     //selecting DOM elements
+const addButton = document.getElementById('add-button');
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
+
+          //creating Add-task function
+function  addTask() {
+    //retrieving & trimming
+    var taskText = taskInput.ariaValueMax.trim();
+
+    //checking if task is empty
+        if (!taskText) {
+            alert('please enter a task!');
+            return;
+        }
+
+        //  Task creation and removal
+const taskItem = document.getElementById('li');
+  taskItem.textContent = taskText;
+
+  const removeBtn = document.createElement('button');
+  removeBtn.textContent = 'Remove';
+  removeBtn.className = 'remove-btn';
+  removeBtn.onclick = function() {
+taskList.removeChild(taskItem)
+  };
+
+taskItem.appendChild(removeBtn);
+taskItem.appendChild(taskItem);
+
+//clearing task input fields
+taskInput.value = '';
+}
+
+//Attaching event listeners
+addButton.addEventListener('click', addTask);
+taskInput.addEventListener('keypress', function(event) {
+    if(event.key === 'Enter') {
+        addTask();
+    }
+});
+});
