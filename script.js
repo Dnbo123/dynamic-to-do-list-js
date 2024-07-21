ecb67944f40177548545e65850f485ccef9e77c7
Don Bosco <don965877@gmail.com>
2024-07-22 01:06:57 +0300

    added last task

--- script.js
+++ script.js
@@ -4,6 +4,39 @@ const addButton = document.getElementById('add-task-btn');
 const taskInput = document.getElementById('task-input');
 const taskList = document.getElementById('task-list');
 
+
+    //Function to load tasks from local storage
+function loadTasks() {
+const tasksFromStorage = local.storage.getItem('tasks');
+let tasks = [];
+if (tasksFromStorage) {
+tasks = JSON.parse(tasksFromStorage);
+}
+tasks.forEach(task => createTaskElement(task));
+}
+
+//Function to create task element in DOM
+function createTaskElement (task) {
+const taskItem = document.createElement('li');
+taskItem.textContent = task;
+
+const removeBtn = document.createElement('button');
+removeBtn.textContent = 'remove';
+removeBtn.className = 'remove-btn';
+removeBtn.onclick = function () {
+taskList.removeChild(taskItem);
+
+//updating task array and local storage on removal
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+const taskIndex = tasks.indexOf(task);
+tasks.splice(taskIndex, 1);
+localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+taskItem.appendChild(removeBtn);
+taskList.appendChild(taskItem);
+}
+
+
           //creating Add-task function
 function  addTask() {
     //retrieving & trimming
@@ -14,6 +47,14 @@ function  addTask() {
             alert('please enter a task!');
             return;
         }
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+tasks.push(taskText);
+localStorage.setItem('tasks', JSON.stringify(tasks));
+
+createTaskElement(taskText);
+taskInput.value = '';
+}
+loadTasks();
 
         //  Task creation and removal
 const taskItem = document.getElementById('li');
